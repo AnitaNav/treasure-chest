8a7e17e9209918d5b0e50e281eb75126d1c74a78
AnitaNav <anitakm215@gmail.com>
2022-12-14 14:02:29 -0800

    Initial setup

--- src/pages/App/App.jsx
+++ src/pages/App/App.jsx
@@ -3,7 +3,7 @@ import { Routes, Route } from 'react-router-dom';
 import { getUser } from '../../utilities/users-service';
 import './App.css';
 import AuthPage from '../AuthPage/AuthPage';
-import NewOrderPage from '../NewOrderPage/NewOrderPage';
+import CartPage from '../CartPage/CartPage';
 import DonatePage from '../DonatePage/DonatePage';
 import NavBar from '../../components/NavBar/NavBar';
 
@@ -17,7 +17,7 @@ export default function App() {
             <NavBar user={user} setUser={setUser} />
             <Routes>
               {/* Route components in here */}
-              <Route path="/orders/new" element={<NewOrderPage />} />
+              <Route path="/orders/new" element={<CartPage />} />
               <Route path="/orders" element={<DonatePage />} />
             </Routes>
           </>
