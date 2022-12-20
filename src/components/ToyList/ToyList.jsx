cbb2cbe97c1da77179cee4c9f08c1e88796d8642
AnitaNav <anitakm215@gmail.com>
2022-12-20 10:22:00 -0800

    CSS update homepage

--- src/components/ToyList/ToyList.jsx
+++ src/components/ToyList/ToyList.jsx
@@ -4,9 +4,9 @@ export default function ToyList({ toy, ItemtoCart, add, remove }) {
   return (
     <>
       
-        <h3>Name: {toy.name} </h3>
         <div className = "Toy-container">
-            <img src={toy.image} alt={toy.name} />
+        <h3 className="Toy-name">Name: {toy.name} </h3>
+            <img className="Image" src={toy.image} alt={toy.name} />
           </div>
         {add && <button className="button" onClick={() => ItemtoCart(toy._id)}>Cart 🛒</button>}
         {remove && <button className="button" onClick={() => ItemtoCart(toy._id)}>Remove ❌</button>}
